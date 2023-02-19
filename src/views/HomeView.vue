4657602497c4dc9b9de50dab885edb2448b6b550
Pierre-Brieuc <pierre-brieuc.collet@edu.devinci.fr>
2023-02-19 21:23:12 +0100

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <Home msg="Welcome to the list of locations, please register or login before access to the list"/>
+    <Home msg="Welcome, please register or login before access to the list"/>
   </div>
 </template>
 
